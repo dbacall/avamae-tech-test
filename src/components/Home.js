cdc3111c4cb3be4b6c00cd69ed71d36551c82fb2
dbacall <dbacall@hotmail.co.uk>
2020-04-26 16:51:18 +0100

    style: style desktpop design of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,41 +30,58 @@ class Home extends Component {
           <Swiper {...params}>
             {this.props.bannerData.map((item) => {
               return (
-                <div>
-                  <h3>{item.Title}</h3>
-                  <h5>{item.Subtitle}</h5>
-                  <Link to="/contact-us">Contact us</Link>
+                <div className="banner">
+                  <div className="banner-info">
+                    <h1>{item.Title}</h1>
+                    <h4>{item.Subtitle}</h4>
+                    <Link to="/contact-us" className="btn-1">
+                      Contact us
+                    </Link>
+                  </div>
+                  <div className="gradient"></div>
                   <img src={item.ImageUrl} className={"banner-img"} />
                 </div>
               );
             })}
           </Swiper>
         )}
-        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus
-          lacinia ultricies. Sed et lobortis magna. Mauris et dolor eget metus
-          molestie interdum. Aliquam urna ante, interdum vel arcu et,
-          consectetur commodo quam. In pharetra scelerisque dui, nec dapibus
-          urna ornare quis. Vivamus eu nisi ultrices, convallis nibh ut, mattis
-          nulla. Mauris et felis id mi dapibus tristique at non dui. Duis
-          suscipit risus nec est lobortis suscipit. Ut et rhoncus mi.
-        </p>
-        <ul>
-          <li key="1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </li>
-          <li key="2">Duis vehicula erat ut ornare vulputate.</li>
-          <li key="3">Duis molestie tortor eu porttitor tristique.</li>
-          <li key="4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </li>
-        </ul>
-        <Link to="/about-us">Learn more</Link>
-        <img src={OfficeImg} alt="Office" className="office-img" />
-        <div>
-          <img src={WomanImg} className="woman-img" />
-          <div>
+        <div className="container1">
+          <div className="flex-item">
+            <h4 className="flex-title">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </h4>
+            <p className="flex-para">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              tempus lacinia ultricies. Sed et lobortis magna. Mauris et dolor
+              eget metus molestie interdum. Aliquam urna ante, interdum vel arcu
+              et, consectetur commodo quam. In pharetra scelerisque dui, nec
+              dapibus urna ornare quis.
+            </p>
+            <ul className="flex-list">
+              <li className="home-li" key="1">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </li>
+              <li className="home-li" key="2">
+                Duis vehicula erat ut ornare vulputate.
+              </li>
+              <li className="home-li" key="3">
+                Duis molestie tortor eu porttitor tristique.
+              </li>
+              <li className="home-li" key="4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </li>
+            </ul>
+            <Link to="/about-us" className="btn-2">
+              Learn more
+            </Link>
+          </div>
+          <div className="flex-item">
+            <img src={OfficeImg} alt="Office" className="office-img" />
+          </div>
+        </div>
+        <div className="container2">
+          <img src={WomanImg} className="banner-img" />
+          <div className="container2-info">
             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -72,46 +89,40 @@ class Home extends Component {
               lacus augue. Integer quis leo dapibus, aliquam urna non, tempus
               purus. Nam rhoncus, magna sed egestas blandit, nisl est placerat
               mi, id condimentum sem elit sed purus. Aenean faucibus neque eu
-              ante dapibus, ullamcorper finibus risus porttitor. Proin
-              condimentum luctus tellus et tincidunt. Nulla dignissim, nisi vel
-              rutrum convallis, risus nunc feugiat nunc, non volutpat arcu sem
-              vel magna.
+              ante dapibus, ullamcorper finibus risus porttitor.
             </p>
-            <Link to="/">Log in</Link>
+            <Link to="/" className="btn-3">
+              Log in
+            </Link>
           </div>
         </div>
-        <div>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-          <h5>
-            Aenean faucibus neque eu ante dapibus, ullamcorper finibus risus
-            porttitor
-          </h5>
-          <p>
-            Vivamus euismod erat et faucibus faucibus. Fusce dictum risus eget
-            purus pulvinar pulvinar id ut tellus. Nullam sed ligula quis nisi
-            dapibus consequat at ac tellus. Pellentesque scelerisque nisi a
-            consectetur ullamcorper. Donec libero sapien, vulputate id ornare
-            in, imperdiet eget urna. Ut malesuada ornare sem, eu cursus magna
-            eleifend ut. Quisque sed magna lectus. Vestibulum gravida varius
-            arcu, a facilisis neque tincidunt a. Ut molestie congue eros, at
-            efficitur nibh fermentum aliquam. Vestibulum rutrum vulputate metus.
-            Pellentesque lobortis rutrum turpis vitae ornare. Maecenas non
-            pretium arcu, a rhoncus metus. Nam tincidunt, risus ac tincidunt
-            hendrerit, purus lectus volutpat sapien, sed blandit nulla diam
-            vitae justo. Suspendisse congue, sem eu iaculis auctor, lectus sem
-            interdum dui, eu laoreet quam erat a augue. <br></br> Nam
-            consectetur nulla eu justo feugiat, in finibus velit dictum. Fusce
-            facilisis vel ex eu feugiat. Fusce vitae maximus sapien. Quisque
-            feugiat hendrerit finibus. Aliquam justo velit, ultrices at
-            hendrerit id, cursus et tortor. Nam sed mauris est. Integer vel
-            rutrum lacus. Morbi eu odio ut dui elementum aliquam ac quis lectus.
-            Ut suscipit metus dui, at aliquet lorem aliquet nec. Praesent non
-            pulvinar massa. Duis hendrerit scelerisque ex eu mollis. Cras
-            pharetra est mi, eget aliquet nisi imperdiet vel. Proin pellentesque
-            nisl sed turpis pulvinar, vel mattis dui mattis. Praesent enim
-            libero, aliquam eget lectus ut, posuere efficitur elit. Nullam nec
-            lorem lorem. Ut vitae ante vitae mi gravida fermentum. Nunc in
-            sagittis mauris. <br></br>
+        <div className="container3">
+          <div className="container3-titles">
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+            <h5>Aenean faucibus neque eu ante dapibus</h5>
+          </div>
+          <p className="container3-paras">
+            <b>
+              Vivamus euismod erat et faucibus faucibus. Fusce dictum risus eget
+              purus pulvinar pulvinar id ut tellus. Nullam sed ligula quis nisi
+              dapibus consequat at ac tellus. Pellentesque scelerisque nisi a
+              consectetur ullamcorper. Donec libero sapien, vulputate id ornare
+              in, imperdiet eget urna. Ut malesuada ornare sem, eu cursus magna
+              eleifend ut. Quisque sed magna lectus. Vestibulum gravida varius
+              arcu, a facilisis neque tincidunt a. Ut molestie congue eros, at
+              efficitur nibh fermentum aliquam.
+            </b>{" "}
+            <br />
+            <br /> Nam consectetur nulla eu justo feugiat, in finibus velit
+            dictum. Fusce facilisis vel ex eu feugiat. Fusce vitae maximus
+            sapien. Quisque feugiat hendrerit finibus. Aliquam justo velit,
+            ultrices at hendrerit id, cursus et tortor. Nam sed mauris est.
+            Integer vel rutrum lacus. Morbi eu odio ut dui elementum aliquam ac
+            quis lectus. Ut suscipit metus dui, at aliquet lorem aliquet nec.
+            Praesent non pulvinar massa. Duis hendrerit scelerisque ex eu
+            mollis. Cras pharetra est mi, eget aliquet nisi imperdiet vel. Proin
+            pellentesque nisl sed turpis pulvinar. <br />
+            <br />
             Pellentesque pretium pharetra viverra. Proin dictum hendrerit
             vestibulum. Sed dapibus varius orci eu sagittis. Ut ullamcorper
             sodales tincidunt. Quisque fringilla, ipsum nec sodales varius, orci
@@ -124,7 +135,9 @@ class Home extends Component {
             non neque nec, fringilla consequat massa. Donec porttitor justo vel
             elementum sagittis.
           </p>
-          <Link to="/contact-us">Contact us</Link>
+          <Link to="/contact-us" className="btn-4">
+            Contact us
+          </Link>
         </div>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,28 @@ import Logo from "../assets/Logo.svg";
 
 export const Navbar = () => {
   return (
-    <ul>
+    <ul className="nav-ul">
       <img src={Logo} className="logo" alt="logo" />
       <div className="nav-btns">
         <li key="login">
-          <Link to="" id="login">
+          <Link to="" className="nav-li" id="login">
             Log in
           </Link>
         </li>
         <li key="contact">
-          <Link to="/contact-us">CONTACT US</Link>
+          <Link className="nav-li" to="/contact-us">
+            CONTACT US
+          </Link>
         </li>
         <li key="about">
-          <Link to="/about-us">ABOUT US</Link>
+          <Link className="nav-li" to="/about-us">
+            ABOUT US
+          </Link>
         </li>
         <li key="home">
-          <Link to="/">HOME</Link>
+          <Link className="nav-li" to="/">
+            HOME
+          </Link>
         </li>
       </div>
     </ul>
