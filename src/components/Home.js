5ff6c12cc4ae7ac27f13eef4b151e5db05cdecde
dbacall <dbacall@hotmail.co.uk>
2020-04-26 10:54:54 +0100

    feat: add slider for home banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 import "../stylesheets/Home.css";
+// import "react-id-swiper/lib/styles/css/swiper.css";
+import Swiper from "react-id-swiper";
+
+const params = {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+};
 
 class Home extends Component {
   render() {
-    return <h1>Home</h1>;
+    return (
+      <Swiper {...params}>
+        <div>Slide #1</div>
+        <div>Slide #2</div>
+        <div>Slide #3</div>
+        <div>Slide #4</div>
+        <div>Slide #5</div>
+      </Swiper>
+    );
   }
 }
 
