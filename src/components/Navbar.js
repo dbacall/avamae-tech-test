0a28d9d83e425568a74a81c2f4dade4a6b413bd9
dbacall <dbacall@hotmail.co.uk>
2020-04-26 10:50:41 +0100

    feat: create navbar with logo and links to all pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../stylesheets/Navbar.css";
+import Logo from "../assets/Logo.svg";
 
 export const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <ul>
+      <li>
+        <img src={Logo} className="logo" alt="logo" />
+      </li>
+      <li>
+        <Link to="/">HOME</Link>
+      </li>
+      <li>
+        <Link to="/about-us">ABOUT US</Link>
+      </li>
+      <li>
+        <Link to="/contact-us">CONTACT US</Link>
+      </li>
+      <li>
+        <Link>Log in</Link>
+      </li>
+    </ul>
+  );
 };
