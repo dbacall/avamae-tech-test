6598e6889cc24ac055fa4cba37a2ef3367c936f2
dbacall <dbacall@hotmail.co.uk>
2020-04-26 14:44:46 +0100

    style: style the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,21 +6,23 @@ import Logo from "../assets/Logo.svg";
 export const Navbar = () => {
   return (
     <ul>
-      <li>
-        <img src={Logo} className="logo" alt="logo" />
-      </li>
-      <li key="home">
-        <Link to="/">HOME</Link>
-      </li>
-      <li key="about">
-        <Link to="/about-us">ABOUT US</Link>
-      </li>
-      <li key="contact">
-        <Link to="/contact-us">CONTACT US</Link>
-      </li>
-      <li key="login">
-        <Link to="">Log in</Link>
-      </li>
+      <img src={Logo} className="logo" alt="logo" />
+      <div className="nav-btns">
+        <li key="login">
+          <Link to="" id="login">
+            Log in
+          </Link>
+        </li>
+        <li key="contact">
+          <Link to="/contact-us">CONTACT US</Link>
+        </li>
+        <li key="about">
+          <Link to="/about-us">ABOUT US</Link>
+        </li>
+        <li key="home">
+          <Link to="/">HOME</Link>
+        </li>
+      </div>
     </ul>
   );
 };
